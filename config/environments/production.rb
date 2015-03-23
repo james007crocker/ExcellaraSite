b0eff418f3f24cccab13173d6b11ebd983268866
James Crocker <jcrocke5@uwo.ca>
2015-03-22 20:22:27 -0400

    Email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,19 @@
 Rails.application.configure do
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  #config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+
+  config.action_mailer.smtp_settings = {
+      address:              'smtp.gmail.com',
+      port:                 587,
+      #domain:              ENV["GMAIL_DOMAIN"],
+      user_name:            ENV["GMAIL_USERNAME"],
+      password:             ENV["GMAIL_PASSWORD"],
+      authentication:       'plain',
+      enable_starttls_auto: true  }
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.

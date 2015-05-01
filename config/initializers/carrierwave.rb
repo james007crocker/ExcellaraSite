9b32d7f506ea84126aa5905937de15ca1e9a38fc
James Crocker <jcrocke5@uwo.ca>
2015-05-01 00:21:52 -0400

    Uncomment Lines and Test

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,7 +1,7 @@
 if Rails.env.production?
   CarrierWave.configure do |config|
-    #config.root = Rails.root.join('tmp')
-    #config.cache_dir = 'carrierwave'
+    config.root = Rails.root.join('tmp')
+    config.cache_dir = 'carrierwave'
     config.fog_credentials = {
         # Configuration for Amazon S3
         :provider              => 'AWS',

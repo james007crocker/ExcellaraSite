2be78f4b28c84eee275ca3905e5f7f1b62885b27
James Crocker <jcrocke5@uwo.ca>
2015-07-29 15:57:20 -0400

    Clockwork Testing #18

--- lib/clock.rb
+++ lib/clock.rb
@@ -36,80 +36,80 @@ end
 # }
 
 #Add :at => time so that this occurs at night
-# every( 2.minute, 'Initiating the MATCH'){
-#   puts "Beginning the Match Automation Process"
-#   User.all.each do |user|
-#     if user.sector == "Accounting" || user.sector == "Human Resources" || user.sector == "Law"
-#       suggestedJob = []
-#       suggestedLink = []
-#       jobs = JobPosting.where(:location => user.location, :sector => user.sector).where("created_at > ?", Date.today() - 7.days).order('created_at ASC')
-#       unless jobs.nil?
-#         jobs.each do |job|
-#           unless Applicant.where(:job_posting_id => job.id, :user_id => user.id)
-#             suggestedJob << job.title + " - " + job.company.name + " - " + job.location + " - " + job.length + " - " + job.hours.to_s + "/week"
-#             suggestedLink << job_posting_url(job)
-#           end
-#           if suggestedJob.size == 3
-#             break
-#           end
-#         end
-#       end
-#       puts user.name + user.lastname
-#       puts "------------------------"
-#       suggestedJob.each do |f|
-#         puts f
-#       end
-#       puts "////////////////////////"
-#       puts " "
-#
-#       if jobtitle.size > 0
-#         UserMailer.jobssuggestedtousers(user.email, user.name, suggestedJob, suggestedLink).deliver_now
-#       end
-#     end
-#   end
-#
-#   JobPosting.all.each do |job|
-#     suggestedUser = []
-#     suggestedLink = []
-#     if job.sector == "Accounting" || job.sector == "Human Resources" || user.sector == "Law"
-#       if job.created_at < Date.today() - 7.days
-#         users = User.where(:location => job.location, :sector => job.sector).where("created_at > ?", Date.today() - 7.days)
-#         users.each do |user|
-#           unless Applicant.where(:job_posting_id => job.id, :user_id => user.id)
-#             suggestedUser << user.name + " " + user.lastname
-#             suggestedLink << user_url(user)
-#           end
-#           if suggestedUser.size == 5
-#             break
-#           end
-#         end
-#       else
-#         users = User.where(:location => job.location, :sector => job.sector).order('created_at ASC')
-#         users.each do |user|
-#           unless Applicant.where(:job_posting_id => job.id, :user_id => user.id)
-#             suggestedUser << user.name + " " + user.lastname + " - " + user.profession + " - " + pluralize(user.years, "Year") + "of  Experience"
-#             suggestedLink << user_url(user)
-#           end
-#           if suggestedUser.size == 5
-#             break
-#           end
-#         end
-#       end
-#       puts job.company.name
-#       puts "----------------"
-#       suggestedUser.each do |f|
-#         puts f
-#       end
-#       puts "//////////////////"
-#       puts " "
-#
-#       if suggestedUser.size > 0
-#         UserMailer.jobssuggestedtocompanies(job.company.email, job.title,  job.company.name, suggestedUser, suggestedLink).deliver_now
-#       end
-#
-#     end
-#   end
-# }
+every( 5.minute, 'Initiating the MATCH'){
+  puts "Beginning the Match Automation Process"
+  User.all.each do |user|
+    if user.sector == "Accounting" || user.sector == "Human Resources" || user.sector == "Law"
+      suggestedJob = []
+      suggestedLink = []
+      jobs = JobPosting.where(:location => user.location, :sector => user.sector).where("created_at > ?", Date.today() - 7.days).order('created_at ASC')
+      unless jobs.nil?
+        jobs.each do |job|
+          unless Applicant.where(:job_posting_id => job.id, :user_id => user.id)
+            suggestedJob << job.title + " - " + job.company.name + " - " + job.location + " - " + job.length + " - " + job.hours.to_s + "/week"
+            suggestedLink << job_posting_url(job)
+          end
+          if suggestedJob.size == 3
+            break
+          end
+        end
+      end
+      puts user.name + user.lastname
+      puts "------------------------"
+      suggestedJob.each do |f|
+        puts f
+      end
+      puts "////////////////////////"
+      puts " "
+
+      if jobtitle.size > 0
+        UserMailer.jobssuggestedtousers(user.email, user.name, suggestedJob, suggestedLink).deliver_now
+      end
+    end
+  end
+
+  # JobPosting.all.each do |job|
+  #   suggestedUser = []
+  #   suggestedLink = []
+  #   if job.sector == "Accounting" || job.sector == "Human Resources" || user.sector == "Law"
+  #     if job.created_at < Date.today() - 7.days
+  #       users = User.where(:location => job.location, :sector => job.sector).where("created_at > ?", Date.today() - 7.days)
+  #       users.each do |user|
+  #         unless Applicant.where(:job_posting_id => job.id, :user_id => user.id)
+  #           suggestedUser << user.name + " " + user.lastname
+  #           suggestedLink << user_url(user)
+  #         end
+  #         if suggestedUser.size == 5
+  #           break
+  #         end
+  #       end
+  #     else
+  #       users = User.where(:location => job.location, :sector => job.sector).order('created_at ASC')
+  #       users.each do |user|
+  #         unless Applicant.where(:job_posting_id => job.id, :user_id => user.id)
+  #           suggestedUser << user.name + " " + user.lastname + " - " + user.profession + " - " + pluralize(user.years, "Year") + "of  Experience"
+  #           suggestedLink << user_url(user)
+  #         end
+  #         if suggestedUser.size == 5
+  #           break
+  #         end
+  #       end
+  #     end
+  #     puts job.company.name
+  #     puts "----------------"
+  #     suggestedUser.each do |f|
+  #       puts f
+  #     end
+  #     puts "//////////////////"
+  #     puts " "
+  #
+  #     if suggestedUser.size > 0
+  #       UserMailer.jobssuggestedtocompanies(job.company.email, job.title,  job.company.name, suggestedUser, suggestedLink).deliver_now
+  #     end
+  #
+  #   end
+  # end
+}
 
 
 
